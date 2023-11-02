c9c32ccb9b6aa7adcbfaa16a2aef2b053ba7b7e0
hasantahir <hasan.tahir.abbas@gmail..com>
2023-11-02 17:31:23 +0000

    new laptop

---
+++ age.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int age = 50;
+    char firstname = 'H';
+    char lastname = 'A';
+    float weight = 100.3;
+    printf("My age is %10, my initials are %c %c, and my weight is %2.8f.\n", age, firstname, lastname, weight); 
+    return 0;
+}
