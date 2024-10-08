0e765083b5dc91d454f772864931e7bde9be6a2e
Mark D. Butala <markbutala@intl.zju.edu.cn>
2024-10-08 15:39:24 +0800

    Made the last printf case a bit more interesting.

--- 2024/src/lecture3/format_specifier_test.c
+++ 2024/src/lecture3/format_specifier_test.c
@@ -5,6 +5,6 @@ int main () {
     printf("Value x=%d and value y=%9.3f\n", x, y);
     printf("Value x=%i and value y=%7.1f\n", x, y);
     printf("Value x=%3d and value y=%5.1f\n", x, y);
-    printf("Value x=%3d and value y=%3.1f\n", x, y);
+    printf("Value x=%1d and value y=%3.1f\n", x, y);
     return 0;
 }
